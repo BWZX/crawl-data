6e332927acc373af97be6d7853365b4bb36ea43a
adajass <jass.ada@qq.com>
2017-05-17 13:04:45 +0800

    djkg

--- publicstuff/mongoModel.py
+++ publicstuff/mongoModel.py
@@ -10,7 +10,7 @@ from pymodm import connect, fields, MongoModel, EmbeddedMongoModel
 
 # Connect to MongoDB first. PyMODM supports all URI options supported by
 # PyMongo. Make sure also to specify a database in the connection string:
-connect('mongodb://124.205.43.244:27017/quant')
+connect('mongodb://node0:27017/quant')
 
 class Exchange(MongoModel):
     name = fields.CharField()
