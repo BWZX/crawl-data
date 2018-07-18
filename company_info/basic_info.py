3461faef13059336b9991eb66381dbb66c2c7f3b
adajass <jass.ada@qq.com>
2018-07-18 21:42:33 +0800

    dkjg

--- company_info/basic_info.py
+++ company_info/basic_info.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 from pyquery import PyQuery as pq
 from pandas import DataFrame as dtf
 import json
+import numpy as np
 from mongoconnect import *
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),'../publicstuff'))
@@ -117,22 +118,22 @@ def fetchHoldFund(stoid):
         ts=ts.decode('gbk')
         # print(ts)
         d = pq(ts)
-        data_table = {}
+        data_table = []
         table = d('table#FundHoldSharesTable')
         for tr in table('tr').items():
-            print(tr('td').text()+' then:\n')
+            # print(tr('td').text()+' then:\n')
             if not tr('td').eq(3):
-                date = tr('td').eq(1).text()
-                data_table[date] = []
+                date = tr('td').eq(1).text()                
                 # print(date)
             if not tr('td').eq(1).text().isnumeric():
                 continue
             cell = []
+            cell.append(date)            
             for td in tr('td').items():
-                cell.append(td.text()) 
+                cell.append(td.text())                 
                 # print(cell)
-
-            data_table[date].append(cell.copy())
+            if len(cell) == 7:
+                data_table.append(cell.copy())
         return data_table
     else:
         raise Exception('crawl failure!')
@@ -227,17 +228,32 @@ def fetchStockStructure(stoid):
     else:
         raise Exception('crawl failure!')
 
-
-if __name__ == '__main__':
+def Finance_Main():
+    import time
     # fetchType(0)  
     # fetchHoldFund('002458')
 
     # print(fetchSummary('002458'))
     # exit()
-    indexxx = config.stolist.index('002327') + 1
+    def drop_except(code):
+        try:
+            summe = fetchSummary(code)  
+            if not summe:
+                time.sleep(10)
+                drop_except(code)
+            else:
+                return summe
+            pass
+        except Exception:
+            time.sleep(3)
+            drop_except(code) 
+            pass
+
+    indexxx = config.stolist.index('601789') +1
     for code in config.stolist[indexxx:]:    #['000422']:  #422
         print(code)
-        finance_sumery = fetchSummary(code)    
+        time.sleep(3)
+        finance_sumery = drop_except(code)  
         # print('\n\n')
         start_year = min(finance_sumery).split('-')[0]
         end_year = max(finance_sumery).split('-')[0]
@@ -270,6 +286,7 @@ if __name__ == '__main__':
             else:
                 continue
             temp = [['date', it]] + temp
+            # print(temp)
             finance.append(temp)
 
         # print(finance)
@@ -289,14 +306,41 @@ if __name__ == '__main__':
             # print(row)
             # print(rown)
             # print(columns)
-            df.loc[len(df)] = row.copy()
+            if len(row) == len(columns):
+                df.loc[len(df)] = row.copy()
+            else:
+               continue
+            
 
         df['code'] = code
         # print(df)
         records = json.loads(df.T.to_json()).values()
         financetable.insert(records)
-    
-    
-    
-    # fetchStockStructure('002458')
+  
 
+if __name__ == '__main__': 
+    import time
+    for code in config.stolist:
+        print(code)
+        hfund = fetchHoldFund(code)
+        hfund_dtf = dtf(hfund,columns=['date','fund_name','fund_code','hold_shares','liquid_share_rate','maket_value','net_value_rate'])
+        hfund_dtf['code'] = code
+        # print(hfund_dtf)
+        # exit()        
+        # print(df)
+        records = json.loads(hfund_dtf.T.to_json()).values()
+        holdfund.insert(records)
+        # struct_data = fetchStockStructure(code)    
+        # ff = dtf(struct_data)  
+        # del ff[0]   
+        # dd = ff.T
+        # dd = dd.as_matrix()
+        # shape = dd.shape
+        # rows = shape[0] * (shape[1]/6)
+        # dd = dd.reshape(int(rows), 6)
+        # columns_name = ['action_date','report_date','general_equity','flow_equity','executive_equity','restrict_equity']
+        # df = dtf(dd,columns=columns_name)
+        # df['code'] = code
+        # records = json.loads(df.T.to_json()).values()
+        # equitystructure.insert(records)
+        # time.sleep(5)
